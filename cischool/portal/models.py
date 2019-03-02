9c0fc6dc2aecf198028af03165f1268bfc9fffbe
Akash Trehan <akash.trehan123@gmail.com>
2019-03-03 02:28:49 +0530

    Added models

--- cischool/portal/models.py
+++ cischool/portal/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+class Venue(models.Model):
+	name = models.CharField(max_length=50)
+	router_ip = models.GenericIPAddressField()
+
+
+class Slot(models.Model):
+	start = models.IntegerField() # 0 - 23 0 means from 00:00 to 00:59
+	day = models.IntegerField() # 0-6 0 is Sunday
+
+
+class Instructor(models.Model):
+	username = models.CharField(max_length=50, primary_key=True)
+	name = models.CharField(max_length=50)
+
+
+class Student(models.Model):
+	username = models.CharField(max_length=50, primary_key=True)
+	name = models.CharField(max_length=50)
+
+
+class Lecture(models.Model):
+	venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+	slot = models.ForeignKey(Slot, on_delete=models.CASCADE)
+
+
+class Rule(models.Model):
+	ip = models.CharField(max_length=15)
+	website = models.URLField()
+	ports = models.CharField(max_length=50)
+	action = models.CharField(max_length=15)
+
+
+class Policy(models.Model):
+	rules = models.ManyToManyField(Rule)
+	instructor = models.ForeignKey(Instructor)
+	white = models.BooleanField()
+	
+
+class Course(models.Model):
+	lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+	instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+	students = models.ManyToManyField(Student)
+	policy = models.ForeignKey(Policy, on_delete=models.DO_NOTHING)
