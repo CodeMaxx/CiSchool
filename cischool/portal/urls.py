1dc49db2ced59860420b07bea3fa199aac4c7984
Arpan Banerjee <arpanbnrj9@gmail.com>
2019-03-03 11:16:09 +0530

    create_policy

--- cischool/portal/urls.py
+++ cischool/portal/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
 	path('dashboard/', views.dashboard, name='dashboard'),
 	path('courses/<int:pk>/edit/', views.edit_course, name='edit_course'),
 	path('mypolicies/', views.policies, name='get_policies'),
-	path('policy/<int:pk>/edit/', views.edit_policies, name='edit_policies'),
+	path('policy/', views.create_policy, name='create_policy'),
+	path('policy/<id>/', views.edit_policies, name='edit_policies'),
 	path('policy/fetch/', views.fetch_policy, name='fetch_policy')
 ]
--- cischool/portal/views.py
+++ cischool/portal/views.py
@@ -9,6 +9,7 @@ import json
 import sys
 import requests
 
+server = "https://fmcrestapisandbox.cisco.com"
 
 # Create your views here.
 
@@ -54,14 +55,17 @@ def login(request):
 def logout(request):
 	logout(request)
 
+
 def register(request):
 	context = {'hello': 'world'}
 	return render(request, 'portal/register.html', context)
+
 
 @login_required
 def dashboard(request):
 	context = {'hello': 'world'}
 	return render(request, 'portal/dashboard.html', context)
+
 
 @login_required
 def courses(request):
@@ -75,25 +79,7 @@ def courses(request):
 def edit_course(request, pk):
 	pass
 
-
-@login_required
-def policies(request):
-	user = request.user
-	instructor = Instructor.objects.get(user=user)
-	policies = Policy.objects.filter(instructor=instructor)
-	return render(request, 'portal/policies.html', context={'policies': policies})
-
-@login_required
-def edit_policies(request, pk):
-	if request.method == 'GET':
-		policy = Policy.objects.get(pk=pk)
-		context = {'policy': policy}
-		return render(request, 'portal/edit_policy.html', context=context)
-	elif request.method == 'POST':
-		raise NotImplementedError
-
-def fetch_policy(request):
-	server = "https://fmcrestapisandbox.cisco.com"
+def get_auth_token(server):
 	username = "nihal.111"
 	password = "Tx6RWzAQ"
 
@@ -122,6 +108,71 @@ def fetch_policy(request):
 	    print ("Error in generating auth token --> " + str(err))
 	    sys.exit()
 
+
+@login_required
+def policies(request):
+	user = request.user
+	instructor = Instructor.objects.get(user=user)
+	policies = Policy.objects.filter(instructor=instructor)
+	return render(request, 'portal/policies.html', context={'policies': policies})
+
+
+@login_required
+def edit_policies(request, id):
+	if request.method == 'GET':
+		policy = Policy.objects.get(pk=pk)
+		context = {'policy': policy}
+		return render(request, 'portal/edit_policy.html', context=context)
+	elif request.method == 'POST':
+		raise NotImplementedError
+
+
+def create_policy(request):
+	if request.method == 'GET':
+		return render(request, 'portal/create_policy.html')
+	elif request.method == 'POST':
+		auth_token = get_auth_token(server)
+		headers['X-auth-access-token'] = auth_token
+
+		api_path = "/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/policy/accesspolicies"    # param
+		url = server + api_path
+		if (url[-1] == '/'):
+		    url = url[:-1]
+
+		post_data = {
+		  "type": "AccessPolicy",
+		  "name": request.POST["name"],
+		  "description": request.POST["description"],
+		  "defaultAction": {
+		    "action": request.POST["defaultAction"]
+		  }
+		}
+
+		try:
+		    # REST call with SSL verification turned off:
+		    r = requests.post(url, data=json.dumps(post_data),
+		                      headers=headers, verify=False)
+		    # REST call with SSL verification turned on:
+		    # r = requests.post(url, data=json.dumps(post_data), headers=headers, verify='/path/to/ssl_certificate')
+		    status_code = r.status_code
+		    resp = r.text
+		    print("Status code is: " + str(status_code))
+		    if status_code == 201 or status_code == 202:
+		        print ("Post was successful...")
+		        json_resp = json.loads(resp)
+		    else:
+		        r.raise_for_status()
+		        print ("Error occurred in POST --> " + resp)
+		except requests.exceptions.HTTPError as err:
+		    print ("Error in connection --> " + str(err))
+		finally:
+		    if r:
+		        r.close()
+		return redirect(reverse("portal:edit_policies"))
+
+
+def fetch_policy(request):
+	auth_token = get_auth_token(server)
 	headers['X-auth-access-token'] = auth_token
 
 	idx = "005056BB-0B24-0ed3-0000-858993527846"
